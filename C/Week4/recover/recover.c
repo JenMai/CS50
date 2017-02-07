d546a931ca5770bcb546794fe340be627e228987
JenniMai <jm.jennimai@gmail.com>
2017-02-07 10:11:57 +0100

    indent

--- C/Week4/recover/recover.c
+++ C/Week4/recover/recover.c
@@ -30,10 +30,10 @@ int main (int argc, char *argv[])
 	/*
 	 * Allocate space for recovering
 	 */
-	uint8_t buffer[512];									// buffer array of size 512 bytes
-	char imgname[8];										// for file names
+	uint8_t buffer[512];							// buffer array of size 512 bytes
+	char imgname[8];							// for file names
 	FILE *outptr = NULL;
-	unsigned int n = 0; 									// JPEG file counter
+	unsigned int n = 0; 							// JPEG file counter
 	
 	/*
 	 * Read forensic file and write jpegs until EOF
@@ -41,17 +41,17 @@ int main (int argc, char *argv[])
 	while (fread( &buffer, 512, 1, inptr ) == 1 )
 	{
 		if (buffer[0] == 0xff && buffer[1] == 0xd8 &&
-			buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) // start of a new jpeg
+			buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) 	// start of a new jpeg
 			{
-				if (outptr != NULL) 						// if already writing a jpeg
+				if (outptr != NULL) 				// if already writing a jpeg
 				{
 					fclose(outptr);
-					sprintf(imgname, "%03i.jpg", n);		// name, open and write a new jpeg
+					sprintf(imgname, "%03i.jpg", n);	// name, open and write a new jpeg
                     outptr = fopen(imgname, "w");
 					fwrite(&buffer, 512, 1, outptr);
 					n++;
 				}
-				else										// if first jpeg encountered
+				else						// if first jpeg encountered
 				{
 					sprintf(imgname, "%03i.jpg", n);
 					outptr = fopen(imgname, "w");
@@ -60,13 +60,13 @@ int main (int argc, char *argv[])
 				}
 			}
 		
-		else if (outptr != NULL)							// already writing a jpeg, but no header found
+		else if (outptr != NULL)					// already writing a jpeg, but no header found
 		{
 			fwrite(&buffer, 512, 1, outptr);
 		}
 		else
 		{
-			continue;										// discard those bytes
+			continue;						// discard those bytes
 		}
 	}
 	
