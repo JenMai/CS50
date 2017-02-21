04e4fdccb25108f1337b2edd85348a1a800c6d66
JenniMai <jm.jennimai@gmail.com>
2017-02-21 13:16:16 +0100

    Update mario.c

--- C/Week1/mario.c
+++ C/Week1/mario.c
@@ -1,13 +1,15 @@
-/**
- * Build stairs such as the first level of Super Mario.
- * Few edits needed (less messy loop conditions, comments)
- */
+    /*
+     * Prints the half pyramid at the end of the first level in Super Mario.
+     */
 
 #include<stdio.h>
 #include<cs50.h>
 
 int main(void)
 {
+    /*
+     * Asks user for height of pyramid.
+     */
     int height;
     do
     {
@@ -16,18 +18,22 @@ int main(void)
     }
     while(height < 0 || height > 23);
     
-    for (int row = 0 ; row < height; ++row)
+    /*
+     * Prints the pyramid according to user's input.
+     */
+    
+    for (int row = 0 ; row < height; ++row)                 //for each row
         {
-            for (int space = row + 1 ; space < height; ++space)
+            for (int space = row ; space < height; ++space) // print 'decreasingly' blank tiles first
             {
                 printf( " " );
             }
-            for (int hash = row + 1 ; hash > -1; --hash)
+            for (int hash = row ; hash >= 0; --hash)        // print 'increasingly' hash tiles
             {
                 printf( "#" );
             }
 
-            printf( "\n" );
+            printf( "#\n" );                                //pyramid has one extra column of tiles
         }
     
 }
